5373b111feadf877cb7a0955c7a1c3f7a8ab06d1
SilverRooney <silver.rooney.dev@gmail.com>
2022-05-18 12:31:47 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Silver on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
